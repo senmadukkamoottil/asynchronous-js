fd6198d055e4888d8ba20f10a2cf739c52f5926a
Sen Madukkamoottil <sentvla@gmail.com>
2025-10-03 20:48:08 -0400

    Promise furction all added to make multiple calls

--- public/orders.js
+++ public/orders.js
@@ -1,10 +1,12 @@
-
+let statusList = axios.get('http://localhost:3000/api/orderStatuses');
+let addressesList = axios.get('http://localhost:3000/api/addresses1');
 let statusArray = [];
-showWaiting();
-axios.get('http://localhost:3000/api/orderStatuses').
-  then(data => {
-      statusArray = data.data;
-      return axios.get('http://localhost:3000/api/orders');
+let addressArray = [];
+Promise.all([statusList, addressesList]).
+  then((response) => {
+    statusArray = response[0].data;
+    addressArray = response[1].data;
+    return axios.get('http://localhost:3000/api/orders');
   }).
   then(data => {
     let orders = [];
@@ -13,7 +15,11 @@ axios.get('http://localhost:3000/api/orderStatuses').
         ...order,
         orderStatus: statusArray.find(statusItem => {
           return statusItem.id === order.orderStatusId;
-        }).description
+        }).description,
+        shippingAddressText: () => {
+          const address = addressArray.find(addressItem => addressItem.id === order.shippingAddress);
+          return `${address.street}, ${address.city}, ${address.state}, ${address.zipCode}`;
+        }
       }
     });
     showOrderList('#order-list', orders);
@@ -24,20 +30,28 @@ axios.get('http://localhost:3000/api/orderStatuses').
   });
 
 
-  // then((orders) => {
-  //   orders = orders;
-  //   return 
-  // }).
-  // then((data) => {
-  //     orders = orders.map(item => {
-  //       ...item,
-  //       orderStatus: data.find((item.orderStatusId === data.id ) => {
-          
-  //       },
-  //     });
-  //     showOrderList('#order-list', orders);
-  // })
 
-  // catch((err) => {
-  //   return showError('#order-list', err)
-  // });
+// let statusArray = [];
+// showWaiting();
+// axios.get('http://localhost:3000/api/orderStatuses').
+//   then(data => {
+//       statusArray = data.data;
+//       return axios.get('http://localhost:3000/api/orders');
+//   }).
+//   then(data => {
+//     let orders = [];
+//     orders = data.data.map(order => {
+//       return {
+//         ...order,
+//         orderStatus: statusArray.find(statusItem => {
+//           return statusItem.id === order.orderStatusId;
+//         }).description
+//       }
+//     });
+//     showOrderList('#order-list', orders);
+//   }).catch((err) => {
+//     return showError('#order-list', err)
+//   }).finally(() => {
+//     hideWaiting();
+//   });
+
